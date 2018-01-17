ba02d3d049dc6a70b3fb3b00fdcb5d04bb026efd
Ashley Hardin <ashleyhardin@ashleys-air.attlocal.net>
2018-01-16 23:33:08 -0500

    working forum with working up/down vote buttons, edit buttons, and delete buttons

--- main.js
+++ main.js
@@ -4,57 +4,73 @@ var i = 0
 button.addEventListener('click', function() {
   var userName = document.getElementById('name').value;
   var post = document.getElementById('message').value;
-  var newTr = document.createElement('tr');
-  newTr.id = i
-  newTr.className = "messageRow"
+  var newDiv = document.createElement('div');
+  newDiv.id = i
+  newDiv.className = "Rtable message"
   // var post = document.createTextNode("<td>"+userName+"</td><td>"+message+"</td>");
-  document.getElementsByTagName('table')[0].append(newTr)
+  document.getElementsByClassName('posts')[0].append(newDiv)
   // .innerHTML("<td>"+userName+"</td><td>"+message+"</td>");
-  var tableData = "<td>" + userName + "</td><td>" + post + "</td><td><button id=upBtn" + i + " class='btn-primary btn-success quality quality-up'>+1</button><button id=dwnBtn" + i + " class='btn-primary btn-danger quality quality-down'>-1</button></td>"
-  newTr = document.getElementById(i)
-  newTr.innerHTML = tableData
-  newTr.setAttribute('qual', 0);
-  // var newBtn = document.createElement('button');
-  // document.getElementById(i).append(newBtn)
-  clickTools(i)
+  var tableData = "<div class='Rtable-cell Rtable-cell-narrow Rtable-cell-content'>" + userName +
+    "</div><div class='Rtable-cell Rtable-cell-wide Rtable-cell-content'>" + post +
+    "</div><div class='Rtable-cell Rtable-cell-narrow'><button id=upBtn" + i +
+    " class='btn-primary btn-success quality quality-up'>+1</button><button id=dwnBtn" + i +
+    " class='btn-primary btn-danger quality quality-down'>-1</button></div><div class='Rtable-cell Rtable-cell-narrow Rtable-cell-empty'><button type='button' id=trash" + i +
+    " class='btn btn-default btn-sm pull-left trash'><span class='glyphicon glyphicon-trash'></span></button><button type='button' id=edit" + i +
+    " class='btn btn-default btn-sm edit'><span class='glyphicon glyphicon-pencil'></span></button></div>"
+  newDiv = document.getElementById(i)
+  newDiv.innerHTML = tableData
+  newDiv.setAttribute('qual', 0);
+  clickTools(i, post)
   i++
 });
 
-clickTools = function(i) {
-    var qualityUpButtons = document.getElementsByClassName('quality-up');
-    var qualityDownButtons = document.getElementsByClassName('quality-down');
-    var btnId
-    var buttonsCount = qualityUpButtons.length;
-    qualityUpButtons[i].addEventListener('click', function() {
-      btnId = this.id;
-      trId = document.getElementById(btnId.slice(-1))
-      trQual = parseInt(trId.getAttribute('qual'))
-      trId.setAttribute('qual', trQual + 1)
-      reorderComments()
-    });
-    qualityDownButtons[i].addEventListener('click', function() {
-        btnId = this.id;
-        trId = document.getElementById(btnId.slice(-1))
-        trQual = parseInt(trId.getAttribute('qual'))
-        if (trQual - 1 > -1) {
-            trId.setAttribute('qual', trQual - 1)
-          } else trId.setAttribute('qual', 0)
+clickTools = function(rowId, post) {
+  var qualityUpButton = document.getElementById('upBtn'+i);
+  var qualityDownButton = document.getElementById('dwnBtn'+i);
+  var trashButton = document.getElementById('trash'+i);
+  var editButton = document.getElementById('edit'+i);
+  var row = document.getElementById(rowId)
+  var qualityUpButtons = document.getElementsByClassName('quality-up');
+  var buttonsCount = qualityUpButtons.length;
+  qualityUpButton.addEventListener('click', function() {
+    trQual = parseInt(row.getAttribute('qual'))
+    row.setAttribute('qual', trQual + 1)
+    console.log(parseInt(row.getAttribute('qual')))
+    reorderComments()
+  });
+  qualityDownButton.addEventListener('click', function() {
+    trQual = parseInt(row.getAttribute('qual'))
+    if (trQual - 1 > -1) {
+      row.setAttribute('qual', trQual - 1)
+    } else row.setAttribute('qual', 0)
+    console.log(parseInt(row.getAttribute('qual')))
+    reorderComments()
+  });
+  trashButton.addEventListener('click', function() {
+    row.remove()
+  });
+  editButton.addEventListener('click', function() {
+    result = window.prompt("Update your post", post);
+    row.children[1].innerHTML = result
+  })
 
-          reorderComments()
-        });
 
-      reorderComments = function() {
-        var qualObj = []
-        for (var j = 0; j < buttonsCount; j++) {
-          qualObj[j] = [j, document.getElementsByClassName("messageRow")[j].getAttribute('qual')]
-        }
-        console.log(qualObj)
-      }
+  reorderComments = function() {
+    var qualObj = {}
+    var items = []
+    for (var j = 0; j < buttonsCount; j++) {
+      qualObj["id"] = j
+      qualObj["quality"] = document.getElementsByClassName("message")[j].getAttribute('qual')
+      items[j] = Object.assign({}, qualObj)
     }
-    // function reorderComments() {
-    //
-    //     document.getElementById("myRedDIV").style.order = "4";
-    //     document.getElementById("myBlueDIV").style.order = "3";
-    //     document.getElementById("myGreenDIV").style.order = "1";
-    //     document.getElementById("myPinkDIV").style.order = "2";
-    // }
+    items.sort(function(a, b) {
+      return b.quality - a.quality;
+    });
+    for (var k = 0; k < buttonsCount; k++) {
+      document.getElementById(items[k].id).style.order = (k)
+      // console.log(items[k].id,document.getElementById(items[k].id).style.order,items[k].quality)
+    }
+  }
+
+
+}
